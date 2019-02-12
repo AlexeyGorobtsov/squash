5a0c1c5e0d8cc1acbce73ce4f431f95681c605fd
agorobtsov <AVGorobtsov@alfabank.ru>
2019-02-12 19:30:51 +0300

    test_five commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './index.css';
 console.log(1);
 console.log(2);
-console.log(3);
-console.log(4);
-console.log(5);
-console.log(6);
+console.log('test5');
+console.log('test5_1');
+console.log('test5_2');
